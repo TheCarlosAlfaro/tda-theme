f33c210fbf1441dfd02e195eb9c1bf8243238785
devbysalas <rgkalin@gmail.com>
2019-03-29 19:50:44 -0700

    Added background img to hero section

--- global-templates/jumbotron.php
+++ global-templates/jumbotron.php
@@ -11,66 +11,58 @@ if (!defined('ABSPATH')) {
 }
 ?>
 
-<div class="jumbotron jumbotron-fluid pb-0">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6 floating-img-container text-center order-12">
-        <img src="<?php the_field("hero_image") ?>" alt="" class="floating-img">
-      </div>
-      <div class="col-md-6 text-md-left text-center order-md-12">
-        <div id="typed-strings">
-          <p><?php the_field("typed-strings-1") ?></p>
-          <p><?php the_field("typed-strings-2") ?></p>
-          <p><?php the_field("typed-strings-3") ?></p>
-        </div>
-        <span id="typed" class="font-weight-bold text-primary"></span>
-        <h1 class="font-weight-bold"><?php the_field("heading") ?></h1>
-        <p class="lead"><?php the_field("subheading") ?></p>
+<div class="jumbotron jumbotron-fluid text-light pb-0 hero--bg-image" style="background-image: 
+    linear-gradient(rgba(20,20,20, .6), rgba(20,20,20, .6)), url('<?php the_field('hero_bg_image'); ?>');">
+    <div class="container">
+        <div class="row">
 
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-          <i class="fa fa-play" aria-hidden="true"></i> VER VIDEO
-        </button>
+            <div class="col-md-12 text-center">
+                <div class="hero-content">
+                    <h1 class="font-weight-bold pt-5"><?php the_field("heading") ?>JOIN US THIS WEEKEND</h1>
+                    <p class="lead pb-3"><?php the_field("subheading") ?>9AM, 10:15AM, 11:45AM & 6pm</p>
 
-        <div class="social">
-          <a href="<?php the_field("facebook_url") ?>" target="_blank">
-            <i class="fa fa-facebook"></i>
-          </a>
-          <a href="<?php the_field("instagram_url") ?>" target="_blank">
-            <i class="fa fa-instagram"></i>
-          </a>
-          <a href="<?php the_field("twitter_url") ?>" target="_blank">
-            <i class="fa fa-twitter"></i>
-          </a>
-          <a href="<?php the_field("youtube_url") ?>" target="_blank">
-            <i class="fa fa-youtube"></i>
-          </a>
-          <a href="<?php the_field("aguilas_cfc_web_url") ?>" target="_blank">
-            <i class="fa fa-globe"></i>
-          </a>
-        </div>
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                        <i class="fa fa-play" aria-hidden="true"></i> VIDEO DE BIENVENIDA
+                    </button>
 
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-          aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-                <!-- <script src="https://fast.wistia.com/embed/medias/902avzvnyy.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_902avzvnyy seo=false videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/902avzvnyy/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" onload="this.parentNode.style.opacity=1;" /></div></div></div></div> -->
-                <div class="iframe-container">
-                  <?php the_field("video_youtube_embed") ?>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
 
-      </div>
+                <div class="social">
+                    <a href="<?php the_field("facebook_url") ?>" target="_blank">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                    <a href="<?php the_field("instagram_url") ?>" target="_blank">
+                        <i class="fa fa-instagram"></i>
+                    </a>
+
+                    <a href="<?php the_field("youtube_url") ?>" target="_blank">
+                        <i class="fa fa-youtube"></i>
+                    </a>
+
+                </div>
+
+
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <!-- <script src="https://fast.wistia.com/embed/medias/902avzvnyy.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_902avzvnyy seo=false videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/902avzvnyy/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" onload="this.parentNode.style.opacity=1;" /></div></div></div></div> -->
+                                <div class="iframe-container">
+                                    <?php the_field("video_youtube_embed") ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
-  </div>
-</div>
+</div> 
--- inc/hooks.php
+++ inc/hooks.php
@@ -5,45 +5,51 @@
  * @package understrap
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+  exit; // Exit if accessed directly.
 }
 
-if ( ! function_exists( 'understrap_site_info' ) ) {
+if (!function_exists('understrap_site_info')) {
   /**
    * Add site info hook to WP hook library.
    */
-  function understrap_site_info() {
-    do_action( 'understrap_site_info' );
+  function understrap_site_info()
+  {
+    do_action('understrap_site_info');
   }
 }
 
-if ( ! function_exists( 'understrap_add_site_info' ) ) {
-  add_action( 'understrap_site_info', 'understrap_add_site_info' );
+if (!function_exists('understrap_add_site_info')) {
+  add_action('understrap_site_info', 'understrap_add_site_info');
 
   /**
    * Add site info content.
    */
-  function understrap_add_site_info() {
+  function understrap_add_site_info()
+  {
     $the_theme = wp_get_theme();
-    
+
     $site_info = sprintf(
       '<a href="%1$s" target="_blank">%2$s</a>',
-      esc_url( __( 'http://www.aguilascfc.org/', 'understrap' ) ),
+      esc_url(__('http://www.tda.org/', 'understrap')),
       sprintf(
         /* translators:*/
-        esc_html__( '© Copyright 2019 Aguilas Centro Familiar Cristiano %s', 'understrap' ), ''
+        esc_html__('© Copyright 2019 TDA Tabernáculo De Adoración %s', 'understrap'),
+        ''
       ),
       sprintf( // WPCS: XSS ok.
         /* translators:*/
-        esc_html__( 'Theme: %1$s by %2$s.', 'understrap' ), $the_theme->get( 'Name' ), '<a href="' . esc_url( __( 'http://understrap.com', 'understrap' ) ) . '">understrap.com</a>'
+        esc_html__('Theme: %1$s by %2$s.', 'understrap'),
+        $the_theme->get('Name'),
+        '<a href="' . esc_url(__('http://understrap.com', 'understrap')) . '">understrap.com</a>'
       ),
       sprintf( // WPCS: XSS ok.
         /* translators:*/
-        esc_html__( 'Version: %1$s', 'understrap' ), $the_theme->get( 'Version' )
+        esc_html__('Version: %1$s', 'understrap'),
+        $the_theme->get('Version')
       )
     );
 
-    echo apply_filters( 'understrap_site_info_content', $site_info ); // WPCS: XSS ok.
+    echo apply_filters('understrap_site_info_content', $site_info); // WPCS: XSS ok.
   }
 }
